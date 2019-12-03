14e0d78e9a6aba3a85500de023bbb6fb955027aa
Eduardo Caetano <eduardo.kaetano@gmail.com>
2019-12-02 22:07:52 -0300

    Exercio da lista de carros

---
+++ ListExercicios/ListExercicios/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listaCarros = new List<string>()
+            {
+                "Celta",
+                "Corsa",
+                "Sandero",
+                "Tempra",
+                "Focus"
+            };
+            listaCarros.ForEach(x => Console.WriteLine(x));
+            Console.ReadLine();
+            
+        }
+    }
+}
