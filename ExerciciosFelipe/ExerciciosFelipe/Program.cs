aaf198d53485c1eadc67c8eb8cc8359daa4ea4e2
Eduardo Caetano <eduardo.kaetano@hotmail.com>
2019-12-09 18:21:16 -0300

    Exercicios 1ºcommit

---
+++ ExerciciosFelipe/ExerciciosFelipe/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosFelipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCICIOS 01 E 02
+            //Console.WriteLine("Por favor, Digite o Seu Nome");
+            //string nome = Console.ReadLine();
+            //Console.WriteLine("*******************************************************************************");
+            //Console.WriteLine($"****Seja bem vindo ao sistema de testes {nome}****");
+            //Console.WriteLine("*******************************************************************************");
+
+
+            // EXERCÍCIOS 03
+            //Console.WriteLine("Por favor, Digite o Seu Nome");
+            //string nome = Console.ReadLine();
+            //Console.WriteLine("Por favor, Digite sua idade");
+            //var idade = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("*******************************************************************************");
+            //if (idade > 18)
+            //{
+            //    Console.WriteLine($"Parabéns {nome} vc é um adulto");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Calma {nome} vc logo será um adulto");
+            //}
+            //Console.WriteLine("*******************************************************************************");
+
+            Console.WriteLine("DIGITE UM TEXTO AQUI:");
+            string texto = Console.ReadLine();
+            int contador = 0;
+            foreach (var item in texto)
+            {
+                contador++;
+            }
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine($"Seu texto con tém {contador} caracteres.");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("*******************************************************************************");
+
+
+            Console.WriteLine("Pressione Uma tecla para sair!");
+            Console.ReadKey(true);
+        }
+    }
+}
